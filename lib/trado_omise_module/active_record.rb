5936ddfbddb80b2a3c26c99c3b26830ddedd02cb
Tom Dallimore <me@tomdallimore.com>
2016-08-11 14:05:00 +0700

    Created new model concern.

--- lib/trado_omise_module/active_record.rb
+++ lib/trado_omise_module/active_record.rb
@@ -3,7 +3,22 @@ module TradoOmiseModule
         extend ActiveSupport::Concern
 
         module ClassMethods
-            
+
+            def has_omise_customer
+                after_create :create_omise_customer_account
+
+                def customer
+                    Omise::Customer.retrieve(omise_id)
+                end
+                
+                def create_omise_customer_account
+                    customer = Omise::Customer.create(
+                    email: email,
+                    description: name
+                    )
+                    self.update_column(:omise_id, customer.id)
+                end
+            end
         end
     end
 end
